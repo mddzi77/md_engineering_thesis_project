5a8e89676b7c63a7bda3524bfb0533094be8eaae
mddzi <md.dziem@gmail.com>
2024-10-09 00:40:17 +0200

    - imported inspect plus

---
+++ Assets/Plugins/InspectPlus/Other/SceneFavoritesHolder.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+using UnityEngine;
+
+namespace InspectPlusNamespace
+{
+	/// <summary>
+	/// No longer used because it's saved in scene and therefore isn't user-specific. Use "Window/Inspect+/Basket" instead.
+	/// </summary>
+	[ExecuteInEditMode]
+	public class SceneFavoritesHolder : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		private void Awake()
+		{
+			EditorApplication.delayCall += () =>
+			{
+				if( this != null && !Application.isPlaying )
+				{
+					Scene scene = gameObject.scene;
+					DestroyImmediate( gameObject );
+					EditorSceneManager.MarkSceneDirty( scene );
+					Debug.Log( "(Inspect+) Removed deprecated SceneFavoritesHolder GameObject from scene: " + scene.name );
+				}
+			};
+		}
+#endif
+	}
+}
---
+++ Assets/Plugins/InspectPlus/README.txt
@@ -0,0 +1,4 @@
+= Inspect+ (v1.9.0) =
+
+Documentation: https://github.com/yasirkula/UnityInspectPlus
+E-mail: yasirkula@gmail.com
