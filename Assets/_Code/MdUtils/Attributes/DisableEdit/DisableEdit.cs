6bab0ebe626cfe6756bd2728b11794a0843f0686
mddzi <md.dziem@gmail.com>
2024-10-14 00:46:20 +0200

    - added layer holders and list version to check if position is drawn

---
+++ Assets/_Code/MdUtils/Attributes/DisableEdit/DisableEdit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MdUtils.Attributes.DisableEdit
+{
+    /// <summary>
+    /// Allow to display an attribute in inspector without allow editing
+    /// </summary>
+    public class DisableEdit : PropertyAttribute {
+        public DisableEdit()
+        {
+
+        }
+    }
+}
---
+++ Assets/_Code/TheLayers/LayerHolder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Drawer;
+using UnityEngine;
+
+namespace TheLayers
+{
+    public class LayerHolder : MonoBehaviour
+    {
+        public List<Pixel> Pixels => _pixels;
+        
+        private LayerConfig _layerConfig;
+        private List<Pixel> _pixels = new();
+
+        public void Init(LayerConfig layerConfig)
+        {
+            _layerConfig = layerConfig;
+        }
+        
+        public void AddPixel(Pixel pixel)
+        {
+            _pixels.Add(pixel);
+        }
+        
+        public void RemovePixel(Pixel pixel)
+        {
+            _pixels.Remove(pixel);
+        }
+    }
+}
