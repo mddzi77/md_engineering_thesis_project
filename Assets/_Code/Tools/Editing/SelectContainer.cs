f2f718b5513614e892568a1e719634eac7b2d527
mddzi <md.dziem@gmail.com>
2025-01-07 03:39:01 +0100

    - working movement

--- Assets/_Code/Tools/Editing/SelectContainer.cs
+++ Assets/_Code/Tools/Editing/SelectContainer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TheLayers;
 using TriInspector;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -8,11 +9,14 @@ namespace Tools.Editing
 {
     public class SelectContainer : MonoBehaviour
     {
-        [SerializeField] private LayerMask selectionLayer;
+        [SerializeField] private int selectionLayer = 3;
         [ReadOnly]
         [SerializeField] private List<GameObject> selectedObjects;
+        [SerializeField] private List<LayerTag> layerTags;
         
         public bool HasSelection { get; private set; }
+        
+        private bool _reparent;
 
         public void SetSelectedObjects(List<GameObject> newSelected)
         {
@@ -25,7 +29,7 @@ namespace Tools.Editing
             selectedObjects.AddRange(newSelected);
             foreach (var selected in selectedObjects)
             {
-                // selected.layer = selectionLayer;
+                selected.layer = selectionLayer;
                 var sprite = selected.GetComponent<SpriteRenderer>();
                 var color = sprite.color;
                 color.a = 0.3f;
@@ -39,6 +43,7 @@ namespace Tools.Editing
             {
                 if (selectedObjects.Contains(selected)) continue;
                 selectedObjects.Add(selected);
+                selected.layer = selectionLayer;
                 var sprite = selected.GetComponent<SpriteRenderer>();
                 var color = sprite.color;
                 color.a = 0.3f;
@@ -64,17 +69,19 @@ namespace Tools.Editing
             HasSelection = false;
             foreach (var selected in selectedObjects)
             {
-                // selected.layer = 0;
+                selected.layer = 0;
                 var sprite = selected.GetComponent<SpriteRenderer>();
                 var color = sprite.color;
                 color.a = .95f;
                 sprite.color = color;
             }
+            ResetParent();
             selectedObjects.Clear();
         }
 
         public void ReParent(Transform parent)
         {
+            _reparent = true;
             foreach (var selected in selectedObjects)
             {
                 selected.transform.parent = parent;
@@ -83,7 +90,9 @@ namespace Tools.Editing
 
         public void ResetParent()
         {
-            
+            if (!_reparent) return;
+            ForEachSelected(SortByTag);
+            _reparent = false;
         }
         
         public void ForEachSelected(Action<GameObject> action)
@@ -92,6 +101,25 @@ namespace Tools.Editing
             {
                 action(selected);
             }
+        }
+
+        private void SortByTag(GameObject obj)
+        {
+            foreach (var layerTag in layerTags)
+            {
+                if (obj.tag.Equals(layerTag.Tag))
+                {
+                    var newParent = LayersManager.Instance.LayerHolders[layerTag.layer].transform;
+                    obj.transform.parent = newParent;
+                }
+            }
+        }
+
+        [Serializable]
+        public class LayerTag
+        {
+            public LayerConfig layer;
+            public string Tag => layer.LayerName;
         }
     }
 }
